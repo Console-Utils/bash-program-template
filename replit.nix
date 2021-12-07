1325ddd6b5e600c53fc64a77137d77097b22979c
Emily Grace Seville <42812113+EmilySeville7cfg@users.noreply.github.com>
2021-12-07 13:02:39 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,14 @@
+# Search packages here: https://search.nixos.org/packages
+
+{ pkgs }: {
+  deps = [
+    # Shells
+    pkgs.fish
+    pkgs.elvish
+    pkgs.bash_5
+
+    # Unix tools
+    pkgs.tldr
+    pkgs.tree
+  ];
+}
