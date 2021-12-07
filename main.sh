f915d940d61ead4c137c5b2aa8ce8210033b4565
Emily Grace Seville <42812113+EmilySeville7cfg@users.noreply.github.com>
2021-12-07 14:13:05 +0000

    Add sample getopt usage

--- main.sh
+++ main.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
 
-echo Hello world!
+help() {
+  echo "usage: main.sh [{-v | --verbose }]"
+}
+
+argv="$(getopt --options v --longoptions verbose -- "$@")"
+
+if (( $? != 0 ))
+then
+  help
+  exit 2
+fi
+
+eval set -- "$argv"
+
+while :
+do
+  case $1 in
+    -v | --verbose)
+      echo "Verbose output enabled. Current Arguments: $argv
+Quick Start Example! App is in Verbose mode!"
+      break
+      ;;
+    --)
+      echo "Current Arguments: $argv
+Quick Start Example!"
+      shift
+      break
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
